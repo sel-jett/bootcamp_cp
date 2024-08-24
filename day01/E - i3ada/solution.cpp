4768791c1cfa65f6810ecfccaef823efda7a194c
Salah Eddine El Jettioui <eljettiouisalaheddine@gmail.com>
2024-08-24 13:04:13 +0100

    done problem D i3ada

--- day01/E - i3ada/solution.cpp
+++ day01/E - i3ada/solution.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-void solve()
-{
+void solve() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    string str;
+    cin >> str;
+
+    int max_len = 1;
+    int current_len = 1;
+
+    for (size_t i = 1; i < str.size(); i++) {
+        if (str[i] == str[i - 1]) {
+            current_len++;
+        } else {
+            max_len = max(max_len, current_len);
+            current_len = 1;
+        }
+    }
+    max_len = max(max_len, current_len);
+    cout << max_len << endl;
+}
+
+int main() {
+    solve();
+    return 0;
 }
